5485c965b7382a6aa3d774586b883c22226ae538
Roman Medvedev <roman.medvedev77@gmail.com>
2023-03-27 23:19:02 -0400

    fix dependancies

--- project-management/src/pages/api/tasks.ts
+++ project-management/src/pages/api/tasks.ts
@@ -1,12 +1,12 @@
 import { ApolloServer } from 'apollo-server-micro';
-import { typeDefs } from '../../graphql/schema';
-import { resolvers } from '../../graphql/resolvers';
+import { schema } from '../../../graphql/schema';
+import { resolvers } from '../../../graphql/resolvers';
 import Cors from 'micro-cors';
 
 const cors = Cors()
 
 const apolloServer = new ApolloServer({
-  typeDefs,
+  schema,
   resolvers,
 });
 
