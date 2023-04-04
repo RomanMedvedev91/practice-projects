cde7e91d117bd53e38db7ca2f94e34671c7d3644
Roman Medvedev <roman.medvedev77@gmail.com>
2023-04-03 23:13:45 -0400

    mern-managment-app/add project mutation

--- mern-manager-app/client/src/pages/Home.jsx
+++ mern-manager-app/client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Clients from "../components/Clients";
 import AddClientsModal from "../components/AddClientsModal";
+import AddProjectModal from "../components/AddProjectModal";
 import Projects from "../components/Projects";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <div className="d-flex gap-3 mb-4">
         <AddClientsModal />
+        <AddProjectModal />
       </div>
       <Projects />
       <hr />
