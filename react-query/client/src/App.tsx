cde0d7b35b4f900a248b711ca40da5986ce2a3d8
Roman Medvedev <roman.medvedev77@gmail.com>
2023-05-26 22:22:27 -0400

    react-query/add query

--- react-query/client/src/App.tsx
+++ react-query/client/src/App.tsx
@@ -1,11 +1,38 @@
-
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+const POSTS = [
+  {id: "1", title: "post 1" },
+  {id: "2", title: "post 2" },
+]
 function App() {
-
+  const queryClient = useQueryClient()
+  const poststQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  })
+  const newPostMutation = useMutation({
+    mutationFn: (title: string) => {
+      return wait(1000).then(() => 
+      POSTS.push({ id: crypto.randomUUID(), title }))
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(['posts'])
+    }
+  })
+  if (poststQuery.isLoading) return <h1>Loading...</h1>
+  if (poststQuery.isError) return <h1>{JSON.stringify(poststQuery.error)}</h1>
   return (
     <>
-      <div>TanStack Query</div>
+      <div>
+        {poststQuery.data.map((post) => (
+          <div key={post.id}>{post.title}</div>
+        ))}
+        <button onClick={() => newPostMutation.mutate("new posts")}>click</button>
+      </div>
     </>
   )
 }
 
+function wait(duration: number) {
+  return new Promise(res => setTimeout(res, duration))
+}
 export default App
--- react-query/client/src/main.tsx
+++ react-query/client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 
 const queryClient = new QueryClient();
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>,
 )
