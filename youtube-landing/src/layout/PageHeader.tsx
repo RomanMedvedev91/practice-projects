0fd85ae82d6aefe3a250b5ba4131d06e3471e8a6
roman <roman.medvedev77@gmail.com>
2023-10-19 16:02:07 -0400

    add logo, custom button components

--- youtube-landing/src/layout/PageHeader.tsx
+++ youtube-landing/src/layout/PageHeader.tsx
@@ -1,8 +1,18 @@
+import { Menu } from "lucide-react";
+import logo from "../assets/Logo.png";
+import { Button } from "../components/Button";
 
 export const PageHeader = () => {
   return (
     <div className="flex gap-10 lg:gap-20 justify-betwwen">
-      <div></div>
+      <div className="flex gap-4 items-center flex-shrink-0">
+        <Button variant="ghost" size="icon">
+          <Menu />
+        </Button>
+        <a href="/">
+          <img src={logo} className="h-6" />
+        </a>
+      </div>
       <div></div>
       <div></div>
     </div>
