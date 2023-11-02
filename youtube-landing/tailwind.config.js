bcad68dc5c9c27605dd8fb4a41042a75685dabfc
roman <roman.medvedev77@gmail.com>
2023-11-02 15:48:19 -0400

    fix tailwind config

--- youtube-landing/tailwind.config.js
+++ youtube-landing/tailwind.config.js
@@ -1,13 +1,10 @@
-import colors from "tailwindcss/colors";
+import colors from "tailwindcss/colors"
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  extend: {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    extend: {
       colors: {
         secondary: {
           DEFAULT: colors.neutral[200],
@@ -15,11 +12,10 @@ export default {
           border: colors.neutral[400],
           text: colors.neutral[500],
           dark: colors.neutral[800],
-        ["dark-hover"]: colors.neutral[900]
-        }
-      }
+          ["dark-hover"]: colors.neutral[900],
+        },
+      },
     },
   },
   plugins: [],
 }
-
