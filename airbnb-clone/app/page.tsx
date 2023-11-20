ff5ad1b330064b410f2d3370b4899a5d649f2394
roman <roman.medvedev77@gmail.com>
2023-11-19 23:05:54 -0500

    add modal, fix hidration error

--- airbnb-clone/app/page.tsx
+++ airbnb-clone/app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <div className="text-3xl font-bold underline">hello</div>
+    <div className="text-3xl font-bold underline pt-[120px]">hello</div>
   )
 }
